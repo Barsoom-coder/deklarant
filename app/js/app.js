3c2320454c9122f350e15a5ddf6a697544e25564
roman_kononov <romankononovdev@mail.ru>
2021-03-06 19:29:27 +0700

    added sections advantages and section-services

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,16 @@
 // // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
 document.addEventListener('DOMContentLoaded', () => {
+	$('.invisible').hide();
 
-	// Custom JS
+	$('.show').on('click', function() {
+		$('.invisible').toggle();
 
-})
+	});
+});
